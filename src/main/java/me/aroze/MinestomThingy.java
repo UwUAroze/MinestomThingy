c8d31117472ec70f3d2c54fd42421ffb5be24240
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 04:43:47 +0100

    Messed up commit names oops. + added chunk generation

--- src/main/java/me/aroze/MinestomThingy.java
+++ src/main/java/me/aroze/MinestomThingy.java
@@ -1,6 +1,7 @@
 package me.aroze;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.coordinate.Point;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerLoginEvent;
@@ -24,6 +25,17 @@ public class MinestomThingy {
 
         // Create the instance
         InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        instanceContainer.setGenerator(unit -> {
+            final Point start = unit.absoluteStart();
+            final Point size = unit.size();
+            for (int x = 0; x < size.blockX(); x++) {
+                for (int z = 0; z < size.blockZ(); z++) {
+                    for (int y = 0; y < Math.min(40 - start.blockY(), size.blockY()); y++) {
+                        unit.modifier().setBlock(start.add(x, y, z), Block.STONE);
+                    }
+                }
+            }
+        });
 
         // Set the ChunkGenerator
         // later:tm: instanceContainer.setChunkGenerator(new ChunkGeneration());
