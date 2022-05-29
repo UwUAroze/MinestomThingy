ff43535e8083d96dfb83b46205731f09942387f6
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 02:05:58 +0100

    Got a working server.. with nothing, at all.

---
+++ src/main/java/me/aroze/ChunkGeneration.java
@@ -0,0 +1,20 @@
+package me.aroze;
+
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.*;
+public class ChunkGeneration {
+
+//    Instance instance = MinestomThingy;
+//    instance.setGenerator(unit -> {
+//        final Point start = unit.absoluteStart();
+//        final Point size = unit.size();
+//        for (int x = 0; x < size.blockX(); x++) {
+//            for (int z = 0; z < size.blockZ(); z++) {
+//                for (int y = 0; y < Math.min(40 - start.blockY(), size.blockY()); y++) {
+//                    unit.modifier().setBlock(start.add(x, y, z), Block.STONE);
+//                }
+//            }
+//        }
+//    });
+
+}
--- src/main/java/me/aroze/MinestomThingy.java
+++ src/main/java/me/aroze/MinestomThingy.java
@@ -1,6 +1,17 @@
 package me.aroze;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.*;
+import net.minestom.server.instance.batch.ChunkBatch;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.coordinate.Pos;
+import net.minestom.server.world.biomes.Biome;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MinestomThingy {
     public static void main(String[] args) {
@@ -9,7 +20,24 @@ public class MinestomThingy {
         System.out.println("Starting thingy...");
 
         MinecraftServer minecraftServer = MinecraftServer.init();
-        minecraftServer.start("0.0.0.0", 25565);
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+
+        // Create the instance
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+
+        // Set the ChunkGenerator
+        // later:tm: instanceContainer.setChunkGenerator(new ChunkGeneration());
+
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+
+        // Start the server on port 25565
+        minecraftServer.start("0.0.0.0", 25000);
 
         System.out.println("Started thingy!");
 
