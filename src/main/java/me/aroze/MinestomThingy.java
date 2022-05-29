19234e6bfb16b9d8b1bbc4754a5edd07e5696259
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 01:47:41 +0100

    Fixed server initialisation

--- src/main/java/me/aroze/MinestomThingy.java
+++ src/main/java/me/aroze/MinestomThingy.java
@@ -2,13 +2,13 @@ package me.aroze;
 
 import net.minestom.server.MinecraftServer;
 
-public class Main {
+public class MinestomThingy {
     public static void main(String[] args) {
 
 
         System.out.println("Starting thingy...");
 
-        MinecraftServer minecraftServer = new MinecraftServer();
+        MinecraftServer minecraftServer = MinecraftServer.init();
         minecraftServer.start("0.0.0.0", 25565);
 
         System.out.println("Started thingy!");
