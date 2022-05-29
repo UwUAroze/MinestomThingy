1723a3661f8a5ff94a56c21c8a38fe4f82ce4c5f
aroze123 <68871455+UwUAroze@users.noreply.github.com>
2022-05-29 01:23:18 +0100

    Created basic Minecraft instance

--- src/main/java/me/aroze/Main.java
+++ src/main/java/me/aroze/Main.java
@@ -1,7 +1,15 @@
 package me.aroze;
 
+import net.minestom.server.MinecraftServer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        System.out.println("Starting thingy...");
+        MinecraftServer minecraftServer = new MinecraftServer();
+        minecraftServer.start("0.0.0.0", 25565);
+        System.out.println("Started thingy!");
+
     }
 }
